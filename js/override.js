f41187f5dba7116df0e6203abced78a3cc815788
Joseph Garner <s3550293@student.rmit.edu.au>
2018-09-23 17:56:07 +1000

    Tweaked the display loading for new matches added Changed from right direction to left in an attemped to smooth out the the motion Changed the width of the buttons and drop down menus Changed User Icon in the top right to a dropdown menu

--- js/override.js
+++ js/override.js
@@ -7,39 +7,25 @@ var matchThree = document.getElementById("MatchThree");
 function matchFound(){
     matches++;
     if(matches == 1){
-        // user.classList.add('splitTwo');
-        // matchOne.classList.add('splitTwo');
         user.style.width = "50%";
         matchOne.style.width = "50%";
-        matchOne.style.right = 0;
+        matchOne.style.left = "50%";
     }
     if(matches == 2){
-        // user.classList.remove('splitTwo');
-        // matchOne.classList.remove('splitTwo');
-        // user.classList.add('splitThree');
-        // matchOne.classList.add('splitThree');
-        // matchTwo.classList.add('splitThree');
         user.style.width = "33.33%";
         matchOne.style.width = "33.33%";
         matchTwo.style.width = "33.33%";
-        matchOne.style.right = '33.33%';
-        matchTwo.style.right = 0;
+        matchOne.style.left = '33.33%';
+        matchTwo.style.left = '66.66%';
     }
     if(matches == 3){
-        // user.classList.remove('splitThree');
-        // matchOne.classList.remove('splitThree');
-        // matchTwo.classList.remove('splitThree');
-        // user.classList.add('splitFour');
-        // matchOne.classList.add('splitFour');
-        // matchTwo.classList.add('splitFour');
-        // matchThree.classList.add('splitFour');
         user.style.width = "25%";
         matchOne.style.width = "25%";
         matchTwo.style.width = "25%";
         matchThree.style.width = "25%";
-        matchOne.style.right = '50%';
-        matchTwo.style.right = '25%';
-        matchThree.style.right = 0;
+        matchOne.style.left = '25%';
+        matchTwo.style.left = '50%';
+        matchThree.style.left = '75%';
     }
 }
 
@@ -49,12 +35,17 @@ function resetMatches(){
     matchOne.style.width = "100%";
     matchTwo.style.width = "50%";
     matchThree.style.width = "33.33%";
-    matchOne.style.right = 0;
-    matchTwo.style.right = 0;
-    matchThree.style.right = 0;
-    // user.classList.remove('splitFour');
-    // matchOne.classList.remove('splitFour');
-    // matchTwo.classList.remove('splitFour');
-    // matchThree.classList.remove('splitFour');
-    
+    matchOne.style.left = 0;
+    matchTwo.style.left = 0;
+    matchThree.style.left = 0;
 }
+
+$(function(){
+  
+    $(".dropdown-game-menu a").click(function(){
+      
+      $(".btn:first-child").text($(this).text());
+       $(".btn:first-child").val($(this).text());
+    });
+  
+  });
