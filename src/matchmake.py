248915d4c9f0da510255f7ca49af57b65eccda66
Arnold D'Silva <arnold.m.dsilva@gmail.com>
2018-10-08 17:25:05 +1100

    Working on Matchmake.py

---
+++ src/matchmake.py
@@ -0,0 +1,17 @@
+# import webapp2
+import requests
+import time
+
+def matchmake(players):
+	for i in range(len(players)):
+		resp = requests.get('https://api.fortnitetracker.com/v1/profile/pc/'+players[i], headers={'TRN-Api-Key' : 'e5d4ace5-28ae-4579-a6d4-fad7b8ad556d'})
+		data = resp.json()
+		print data["epicUserHandle"]
+		print data["accountId"]
+		print data["platformNameLong"]
+		print data["lifeTimeStats"]
+		return
+
+
+players = ['aox_', 'Ninja', 'GassyKraken', 'Soul-Cake', 'Muselk']
+matchmake(players)
