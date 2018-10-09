4f78b090d9962bfaf23b31abe34ad4ce894ef647
Arnold D'Silva <arnolddsilva@localhost.localdomain>
2018-10-09 14:16:07 +1100

    Please freaking work

--- squadUp.py
+++ squadUp.py
@@ -1,5 +1,4 @@
 # [START app]
-
 import os
 import urllib
 import logging
@@ -9,52 +8,102 @@ from google.appengine.api import users
 from google.appengine.ext import ndb
 
 from flask import Flask, render_template, request
-
-# import jinja2
-# import webapp2
-# import MySQLdb
-
+from flask_sqlalchemy import SQLAlchemy
 # [END imports]
 
-# [START create_app]
-app = Flask(__name__)
-# [END create_app]
+# [DATABASE info]
 
-# These environment variables are configured in app.yaml.
 CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
 CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
 CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+CLOUDSQL_DATABASE = os.environ.get('CLOUDSQL_DATABASE')
 
-def connect_to_cloudsql():
-    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-        cloudsql_unix_socket = os.path.join(
-            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+LIVE_SQLALCHEMY_DATABASE_URI = (
+    'mysql+pymysql://{user}:{password}@3306/{database}'
+    '?unix_socket=/cloudsql/{connection_name}').format(
+        user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
+        database=CLOUDSQL_DATABASE, connection_name=CLOUDSQL_CONNECTION_NAME)
 
-        db = MySQLdb.connect(
-            unix_socket=cloudsql_unix_socket,
-            user=CLOUDSQL_USER,
-            passwd=CLOUDSQL_PASSWORD)
+if os.environ.get('GAE_INSTANCE'):
+    SQLALCHEMY_DATABASE_URI = LIVE_SQLALCHEMY_DATABASE_URI
 
-        logging.critical('We have connnected to squadup sql')
-    else:
-        db = MySQLdb.connect(host='35.197.191.91', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+# [END DATABASE info]
 
-    return db
+
+# [START create_app]
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = LIVE_SQLALCHEMY_DATABASE_URI
+db = SQLAlchemy(app)
+# [END create_app]
+
+class SquadUser(db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    Email = db.Column(db.String(255), nullable=True)
+    EpicUserHandle = db.Column(db.String(255), nullable=True)
+    AccountId = db.Column(db.String(255), nullable=True)
+    SoloRating = db.Column(db.Integer, nullable=True)
+    DuoRating = db.Column(db.Integer, nullable=True)
+    SquadRating = db.Column(db.Integer, nullable=True)
+
+    def __repr__(self):
+        return '<Email %r>' % self.email
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    user = users.get_current_user()
+    email = ''
+    fhandle = ''
+    if user:
+        url = users.create_logout_url('/')
+        url_linktext = 'Logout'
+        email = user.email()
+        
+        logging.critical(SquadUser.query.all())
+    else:
+        url = users.create_login_url('/')
+        url_linktext = 'Login'
 
-@app.route('/profile/')
+    return render_template('index.html', user=user, email=email, url=url, url_linktext=url_linktext)
+
+@app.route('/profile')
 def profile():
-    return 'Profile Page'
+    return render_template('profile.html')
 
 @app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
+
 # [END app]
+
+
+
+
+# import jinja2
+
+# import MySQLdb
+
+# # These environment variables are configured in app.yaml.
+# CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+# CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+# CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+
+# def connect_to_cloudsql():
+#     if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+#         cloudsql_unix_socket = os.path.join(
+#             '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+
+#         db = MySQLdb.connect(
+#             unix_socket=cloudsql_unix_socket,
+#             user=CLOUDSQL_USER,
+#             passwd=CLOUDSQL_PASSWORD)
+
+#         logging.critical('We have connnected to squadup sql')
+#     else:
+#         db = MySQLdb.connect(host='35.197.191.91', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+
+#     return db
 
 # JINJA_ENVIRONMENT = jinja2.Environment(
 #     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
