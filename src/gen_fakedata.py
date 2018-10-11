ac268a4924825f39713933600a53d441d9cf42b4
Joseph Garner <s3550293@student.rmit.edu.au>
2018-10-11 15:54:18 +1100

    Fixing autoGen

--- src/gen_fakedata.py
+++ src/gen_fakedata.py
@@ -18,9 +18,11 @@ def gen_fakedata():
 		fortniteName = names.get_first_name() + '_' + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
 		score = random.randint(1000, 4500000)
 		squadRating = random.randint(500, 4500)
+		MatchesPlayed = random.randint(1, 10000)
+		winRate = random.randint(0, 100)
 
-		print '%s,%s,%s,%i,%i,%i\n' % (email, fortniteName, accountId, score, duoRating, squadRating)
-		f.write('%s,%s,%s,%i,%i,%i\n' % (email, fortniteName, accountId, score, duoRating, squadRating))
+		print '%s,%s,%s,%i,%i,%i,%i,%i\n' % (email, fortniteName, accountId, score, duoRating, squadRating, MatchesPlayed,winRate)
+		f.write('%s,%s,%s,%i,%i,%i,%i,%i\n' % (email, fortniteName, accountId, score, duoRating, squadRating, MatchesPlayed,winRate))
 		inc = inc + 1
 
 	f.close()
